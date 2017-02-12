c9defb9053a4744dce57ced8a9f95c0f8f0d3812
Jenny Lim <jlimetc@gmail.com>
2017-02-12 14:12:41 -0500

    first jquery edits

--- week-1/HTML:CSS/js/script.js
+++ week-1/HTML:CSS/js/script.js
@@ -1,3 +1,15 @@
 /* $ means it's jquery. 
 */
-$(".red").addClass("yellow bigbox")
+$(".red").addClass("yellow bigbox");
+
+// you can click anywhere on page to get message because selected all divs
+// $("div").on("click",function(){alert("what up dog")}); 
+
+$(".red").click(function(){
+	$(this).toggleClass("orange")
+	//above line = $(".red").click(function(){}
+});
+// toggle will add and remove.
+/*"this" refers to whatever the current instance is. so if you have a bunch of
+	nested functions or nested objects, you can just say "this"
+	If you ever get confused, check out console.log(this)
