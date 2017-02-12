dc59d2c4d522bac919375a83bbcc2b7e8cd096f9
jlimetc <jlimetc@gmail.com>
2017-02-12 14:59:11 -0500

    next level of work

---
+++ week-1/jQuery/divided-times-obfuscated/js/script.js
@@ -0,0 +1,17 @@
+//A button click to reveal drawer with links for each nav item
+	//change text color when category highlighted
+//slide up and slide down
+//adding classes to the buttons
+
+//classes: active, open, slide-down, slide-up
+
+
+//once page loads, then run:
+$(document).ready(function(){
+
+
+
+
+
+
+})
