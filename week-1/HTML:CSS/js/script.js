6910e2e02784ff938420516e03d92ca8659adf9b
paulaleonvendagar <pvl215@nyu.edu>
2017-02-12 15:00:37 -0500

    Imagine this is an explanation of my changes

---
+++ week-1/HTML:CSS/js/script.js
@@ -0,0 +1,11 @@
+$(".red").addClass("bigbox");
+// .removeClass()
+
+//$$("div").on("click")
+
+$(".red").click(function(){
+	$(this).toggleClass("orange");
+	//$(".red").toggleClass("orange")
+});
+
+//$(".red").toggleClass("orange");
