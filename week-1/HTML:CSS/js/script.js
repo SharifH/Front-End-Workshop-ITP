19641ec576b58e7316983c44fbd1072e7605df4b
seyoung <designseyoung@gmail.com>
2017-02-12 14:57:14 -0500

    start making a web and writing js
    “The divided news” javascript

--- week-1/HTML:CSS/js/script.js
+++ week-1/HTML:CSS/js/script.js
@@ -1,1 +1,10 @@
-$
+$(".red").addClass("bigbox"); //adding 2 classes
+
+$(".red").on("click", function(){
+  $(this).toggleClass("orange");
+  // interchangeable with $(".red").toggleClass("orange");
+}); //not calling a function, just write down the function
+
+// $(".red").on("click", function(){}) = $(".red").click(function(){}) ->1time anonymous function.
+// = $(".red").on("click", something());
+//   function something() {}
---
+++ week-1/jQuery/divided-times-obfuscated/js/script.js
@@ -0,0 +1,11 @@
+// A button click to lead to a drawer appearing with links for each nav item
+// slide up and slide down
+// adding classes to the buttons
+// active open slide-down slide up classes
+
+$(document).ready(function(){
+  //everything lives in here!
+
+
+
+})
