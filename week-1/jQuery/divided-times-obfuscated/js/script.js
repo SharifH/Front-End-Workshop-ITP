1c79c314ab17b2928a519753109a9ba175b0cc5b
RSofiaC <itpstudent@172-16-245-27.DYNAPOOL.NYU.EDU>
2017-02-12 15:54:46 -0500

    Syntax problem solved
    No space between $ and (
    no . before active and open because they are pseudo classes

--- week-1/jQuery/divided-times-obfuscated/js/script.js
+++ week-1/jQuery/divided-times-obfuscated/js/script.js
@@ -7,17 +7,17 @@ $(document).ready(function(){
   //International
   $('#primary-nav .international').on('click',function(e){
     e.preventDefault();
-    $ ('#primary-nav .international').siblings().removeClass('active');
+    $('#primary-nav .international').siblings().removeClass('active');
     $('#primary-nav .international').toggleClass('active');
-    if ($('#primary-nav .international').hasClass('.active')){
+    if ($('#primary-nav .international').hasClass('active')){
       if ($('#slide-down').hasClass('open')){
       $('#slide-down .drop.open').hide().removeClass('open');
-      $('.drop.international').show().addClass('.open');
-      console.log('this is working?')
+      $('.drop.international').show().addClass('open');
+      console.log('interantional working?')
     } else {
       $('#slide-down .drop').hide();
-      $ ('.drop.international').show().addClass('open');
-      $ ('#slide-down').slideDown(200).addClass('open');
+      $('.drop.international').show().addClass('open');
+      $('#slide-down').slideDown(200).addClass('open');
     }
   }else{
     $('#slide-down').slideUp(200).removeClass('open');
@@ -25,19 +25,19 @@ $(document).ready(function(){
 });
 
   //Politics
-  $ ('#primary-nav .politics').on('click',function(e){
+  $('#primary-nav .politics').on('click',function(e){
     e.preventDefault();
-    $ ('#primary-nav .politics').siblings().removeClass('active');
+    $('#primary-nav .politics').siblings().removeClass('active');
     $('#primary-nav .politics').toggleClass('active');
-    if($('#primary-nav .politics').hasClass('.active')){
+    if($('#primary-nav .politics').hasClass('active')){
       if ($('#slide-down').hasClass('open')) {
         $('#slide-down .drop.open').hide().removeClass('open');
-        $('.drop.politics').show().addClass('.open');
+        $('.drop.politics').show().addClass('open');
         console.log('this is sibling Politics working')
       }else{
         $('#slide-down .drop').hide();
-        $ ('.drop.politics').show().addClass('open');
-        $ ('#slide-down').slideDown(200).addClass('open');
+        $('.drop.politics').show().addClass('open');
+        $('#slide-down').slideDown(200).addClass('open');
       }
     } else{
       $('#slide-down').slideUp(200).removeClass('open');
