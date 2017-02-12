9cbc84f40b480a889cfd81824b9445760e41e4b9
jlimetc <jlimetc@gmail.com>
2017-02-12 14:27:13 -0500

    file changes

--- week-1/HTML:CSS/js/script.js
+++ week-1/HTML:CSS/js/script.js
@@ -1,15 +1,24 @@
 /* $ means it's jquery. 
 */
+
+
 $(".red").addClass("yellow bigbox");
 
-// you can click anywhere on page to get message because selected all divs
-// $("div").on("click",function(){alert("what up dog")}); 
 
+
+// you can click anywhere on page to get message because selected all divs
+// $("div").on("click",function(){alert("what up dog")});
+//because .on("click", function(){ is so common, there is also
+	//.click(function){       which we use below.
+
+
+
+	//note that we are using anonymous function below! doesn't save anything.
 $(".red").click(function(){
 	$(this).toggleClass("orange")
-	//above line = $(".red").click(function(){}
+	//above line is the same as $(".red").click(function(){}
 });
 // toggle will add and remove.
-/*"this" refers to whatever the current instance is. so if you have a bunch of
-	nested functions or nested objects, you can just say "this"
+/*"this" refers to whatever the current instance is.
 	If you ever get confused, check out console.log(this)
+	Note that "this" tends to confuse others. So only use sparingly, maybe nested functions
